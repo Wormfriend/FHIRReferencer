b4ae582b7e6e2e75a6159421c9fd56b0250a4f4b
voigtfr <frederik.voigt@uniklinik-freiburg.de>
2025-04-24 16:53:40 +0200

    Added requirements.txt

--- fhir_referencer/__init__.py
+++ fhir_referencer/__init__.py
@@ -7,3 +7,5 @@ argparser = ArgumentParser(
 )
 argparser.add_argument("-p", "--profiles", type=str, help="Path to the profiles directory")
 argparser.add_argument("-r", "--resources", type=str, help="Path to the resources directory")
+argparser.add_argument("-o", "--output", type=str, help="Filename of the plantuml output")
+argparser.add_argument("-n", "--diagram-name", type=str, help="Name of the diagram")
